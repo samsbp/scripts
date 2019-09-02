a546b02954a91617f37147055bf721c76caa5cbc
sam.p <samsbp18@gmail.com>
2019-09-02 20:12:36 +0530

    gobuster integrated

--- amass_buster.py
+++ amass_buster.py
@@ -3,59 +3,61 @@ import json,os,subprocess,time,requests,argparse
 parser = argparse.ArgumentParser(description='Script for integrating amass and gobuster into')
 parser.add_argument("-d","--domains", help="domains",required=True)
 args = parser.parse_args()
-domain=args.domains
-f=open('/Users/sam-8402/Desktop/sample/'+domain+'.json','r').read()
-data=f.split('\n')[:-1]
-host={}
-ipList={}
-for d in data:
-    j=json.loads(d)
-    subdomain = j['name']
-    subdomain_ip=[]
+domains=args.domains.split(',')
+for domain in domains:
 
-    portList=[]
-    host[subdomain]={}
-    print 'collecting ports for '+subdomain
-    for ip in j['addresses']:
-        ip_address=ip['ip']
-        if(':' not in ip_address):
-            subdomain_ip.append(ip_address)
+    f=open('***'+domain+'.json','r').read()
+    data=f.split('\n')[:-1]
+    host={}
+    ipList={}
+    for d in data:
+        j=json.loads(d)
+        subdomain = j['name']
+        subdomain_ip=[]
+
+        portList=[]
+        host[subdomain]={}
+        print 'collecting ports for '+subdomain
+        for ip in j['addresses']:
+            ip_address=ip['ip']
+            if(':' not in ip_address):
+                subdomain_ip.append(ip_address)
 
 
-            print ip_address
-            #print 'curl https://www.shodan.io/host/'+ip_address+" | grep \"<div class=\\\"port\\\">\" | sed 's/<div class=\"port\">//g' | sed 's@</div>@@g'"
-            if(ip_address not in ipList.keys()):
-                ports=subprocess.check_output("curl -s https://www.shodan.io/host/"+ip_address+" | grep \"<div class=\\\"port\\\">\" | sed 's/<div class=\"port\">//g' | sed 's@</div>@@g'",shell=True)
-                ports=ports.split('\n')[:-1]
-                time.sleep(1)
-                #print ports,'\n'
+                print ip_address
+                #print 'curl https://www.shodan.io/host/'+ip_address+" | grep \"<div class=\\\"port\\\">\" | sed 's/<div class=\"port\">//g' | sed 's@</div>@@g'"
+                if(ip_address not in ipList.keys()):
+                    ports=subprocess.check_output("curl -s https://www.shodan.io/host/"+ip_address+" | grep \"<div class=\\\"port\\\">\" | sed 's/<div class=\"port\">//g' | sed 's@</div>@@g'",shell=True)
+                    ports=ports.split('\n')[:-1]
+                    time.sleep(1)
+                    #print ports,'\n'
 
-                ipList[ip_address]=ports
-            else:
-                print "ports are already computed"
-                ports=ipList[ip_address]
-            portList+=ports
+                    ipList[ip_address]=ports
+                else:
+                    print "ports are already computed"
+                    ports=ipList[ip_address]
+                portList+=ports
 
-    host[subdomain]['ips']=subdomain_ip
-    host[subdomain]['ports']=list(set(portList))
-    print host[subdomain]['ports'],"\n"
-host['ip_ports']=ipList
+        host[subdomain]['ips']=subdomain_ip
+        host[subdomain]['ports']=list(set(portList))
+        print host[subdomain]['ports'],"\n"
+    host['ip_ports']=ipList
 
-for d in data:
-    j=json.loads(d)
-    subdomain = j['name']
-    ports=host[subdomain]['ports']
-    for port in ports:
-        try:
-            url="http://"+subdomain+":"+port
-            req_http=requests.head(url,verify=False)
+    for d in data:
+        j=json.loads(d)
+        subdomain = j['name']
+        ports=host[subdomain]['ports']
+        for port in ports:
+            try:
+                url="http://"+subdomain+":"+port
+                req_http=requests.head(url,verify=False,timeout=5)
 
-            if(req_http.status_code==400):
-                url="https://"+subdomain+":"+port
-                req_https=requests.head(url)
-                if(req_https.status_code==400):
-                    break
-            print "gobuster: "+url
-            print subprocess.check_output("gobuster dir -u "+url+" -w common_quick.txt -t 50 -k --timeout=10s -s 200,301",shell=True)
-        except:
-            continue
+                if(req_http.status_code==400):
+                    url="https://"+subdomain+":"+port
+                    req_https=requests.head(url,timeout=5)
+                    if(req_https.status_code==400):
+                        break
+                print "gobuster: "+url
+                print subprocess.check_output("gobuster dir -u "+url+" -w common_quick.txt -t 50 -k --timeout=10s -s 200,301",shell=True)
+            except:
+                continue
